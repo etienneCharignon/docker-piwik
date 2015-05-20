d6b5ef4b637f24e87c9f2099b9b0ac8e4b02a14d
Étienne Charignon <etienne.charignon@gmail.com>
2015-05-20 16:56:01 +0200

    remove trailing space

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ set -e
 [ -f "/app/config/global.php" ] || rsync -a --delete /app/config-orig/ /app/config/
 chown -R www-data:www-data /app/config
 /etc/init.d/apache2 start
-tail -F /var/log/apache2/* 
+tail -F /var/log/apache2/*
